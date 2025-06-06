e9c9cbfb8f0101120cd79c39abbd6fbf42edf56f
Sachinkr <royalsachin1530@gmail.com>
2025-06-07 00:38:10 +0530

    Add files via upload

---
+++ chapter-15(Js-Basics-1)/index.js
@@ -0,0 +1,101 @@
+       // cmnt in javascript
+       //  console.log('Namste duniya version 4' );
+       //  let a=5;
+       //  console.log(a);  
+       //  let b="Sachin";
+       //  console.log(b);
+       //  var c=6;
+       //  console.log(c);
+       //  var c=7;
+       //  console.log(c);
+       //  const num=12;
+       //  console.log(num);
+       //  let lastname="Sachin";
+       //  console.log(lastname);
+       //  lastname=3;
+       //  console.log(lastname);
+
+
+       //let a=2**10; //star are working to showing the power of the value
+      // console.log(a);
+
+//       console.log(2>3);
+//       console.log(3==3);
+       
+// Ternary
+
+
+// let age=17;
+// let status=(age>18)?'yes vote': 'no vote';
+// console.log(status);
+
+
+//  if else if else
+
+// let marks=79;
+// if (marks >=90){
+//        console.log('A');
+// }
+// else if(marks>=80){
+//        console.log('B');
+// }
+// else if(marks>=70){
+//        console.log('C');
+// }
+// else if(marks>=60){
+//        console.log('D');
+// }
+// else{
+//        console.log('fail');
+// }
+
+
+//  switch case
+
+// let num=2;
+// switch (num) {
+//        case 1:
+//               console.log('A');
+//               break;
+//         case 2:
+//               console.log('B');
+//               break;
+//         case 3:
+//               console.log('C');
+//               break;
+
+
+//        default:
+//               console.log('E');
+//               break;
+// }
+
+
+
+//  for loop
+
+// for(let i=0;i<5;i++)
+// {
+//        console.log(i);
+// }
+
+
+
+// while loop
+
+
+// let x=1;
+// while (x<=5)
+// {
+//        console.log(x);
+//        x++;
+// }
+
+
+//  Do while loop
+
+let y=1;
+do{
+       console.log(y);
+       y++;
+}while(y<10);
