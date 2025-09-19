b2d45f1661b8a29683100ec0e3a18f48a16babe3
Sachinkr <royalsachin1530@gmail.com>
2025-09-20 00:08:27 +0530

    Add files via upload

---
+++ chapter-37_(react-Advanced-2)/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination from "./Pagination";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+export default function App() {
+
+  const {fetchBlogPosts} = useContext(AppContext);
+  useEffect(() =>{
+    fetchBlogPosts();
+  },[])
+  
+  return (
+    <div className="w-full h-full flex flex-col gap-y-1  justify-center items-center">
+       <Header/>
+       <Blogs/>
+       <Pagination/>
+    </div>
+  );
+}
